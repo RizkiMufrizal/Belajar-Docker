a7f88c641226cce862b60745d5be4a65e748d92a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-16 21:17:46 +0700

    initial project

---
+++ src/main/java/com/rizki/mufrizal/belajar/docker/BelajarDockerApplication.java
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.belajar.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarDockerApplication.class, args);
+	}
+}
