aa9f79d83a966da3c2a773d8dd84972eef7b7b9e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-17 02:19:47 +0700

    update main class

--- src/main/java/com/rizki/mufrizal/belajar/docker/BelajarDockerApplication.java
+++ src/main/java/com/rizki/mufrizal/belajar/docker/BelajarDockerApplication.java
@@ -3,10 +3,19 @@ package com.rizki.mufrizal.belajar.docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * @Since 16 July 2016
+ * @Time 22:02
+ * @Project Belajar-Docker
+ * @Package com.rizki.mufrizal.belajar.docker
+ * @File BelajarDockerApplication
+ */
 @SpringBootApplication
 public class BelajarDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BelajarDockerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarDockerApplication.class, args);
+    }
+
 }
