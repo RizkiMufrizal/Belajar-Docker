5f306eef9bd148166393a65503714f6c399bdc3e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-16 23:08:06 +0700

    tambah enum jenis barang

---
+++ src/main/java/com/rizki/mufrizal/belajar/docker/domain/JenisBarang.java
@@ -0,0 +1,13 @@
+package com.rizki.mufrizal.belajar.docker.domain;
+
+/**
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * @Since 16 July 2016
+ * @Time 22:06
+ * @Project Belajar-Docker
+ * @Package com.rizki.mufrizal.belajar.docker.domain
+ * @File JenisBarang
+ */
+public enum JenisBarang {
+    cair, gas, padat
+}
