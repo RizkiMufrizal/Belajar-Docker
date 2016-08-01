8064a9ce5594994f78b0af3763c2c34983e27b2e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-01 07:29:23 +0700

    update konfigurasi models

--- Belajar-Docker-SailsJS/config/models.js
+++ Belajar-Docker-SailsJS/config/models.js
@@ -10,23 +10,6 @@
  */
 
 module.exports.models = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  // connection: 'localDiskDb',
-
-  /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-  // migrate: 'alter'
-
+  connection: 'MariadbServer',
+  migrate: 'safe'
 };
