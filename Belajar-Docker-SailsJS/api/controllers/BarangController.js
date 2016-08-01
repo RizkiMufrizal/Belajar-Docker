074a5a55b86f5e542d7d3f64ef3d9675a6f50f92
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-01 07:30:56 +0700

    tambah barang Controllers

---
+++ Belajar-Docker-SailsJS/api/controllers/BarangController.js
@@ -0,0 +1,10 @@
+/**
+ * BarangController
+ *
+ * @description :: Server-side logic for managing barangs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
