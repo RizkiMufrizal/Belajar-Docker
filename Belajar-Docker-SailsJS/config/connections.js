72bccfde915faeb0bf2bc08a56d6478b15deb271
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-01 07:28:03 +0700

    tambah konfigurasi koneksi ke mariadb

--- Belajar-Docker-SailsJS/config/connections.js
+++ Belajar-Docker-SailsJS/config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  MysqlServer: {
+    adapter: 'sails-mysql',
+    host: process.env.MARIADB_PORT_3306_TCP_ADDR,
+    user: process.env.MARIADB_ENV_MYSQL_USER,
+    password: process.env.MARIADB_ENV_MYSQL_PASSWORD,
+    database: process.env.MARIADB_ENV_MYSQL_DATABASE
+  },
 
   /***************************************************************************
   *                                                                          *
